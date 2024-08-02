a611f52c5f64e51d51352e3219f1a7165634319f
Turdıbek Jumabaev <turdibekjumabaev05@gmail.com>
2024-08-02 18:57:17 +0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Special opportunities for the Karakalpak language'
+
+# Setting up
+setup(
+    name="jaziw",
+    version=VERSION,
+    author="Turdıbek Jumabaev",
+    author_email="<turdibekjumabaev05@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['karakalpak', 'language', 'python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ]
+)
